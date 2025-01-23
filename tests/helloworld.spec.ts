7e92f1cfaf45c3113fc890f7841083f39ae270c7
Reed Anderson <reed.forestmonster@gmail.com>
2025-01-23 06:21:42 -0800

    Update test branch url

--- tests/helloworld.spec.ts
+++ tests/helloworld.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const baseUrl = process.env.DEPLOY_URL || 'https://pedigreesync.netlify.app/';
+const baseUrl = process.env.BRANCH_URL || 'https://pedigreesync.netlify.app/';
 
 test('NavBar brand logo is present', async ({ page }) => {
   await page.goto(baseUrl);
