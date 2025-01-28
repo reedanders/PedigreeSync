fefc7eac9ef57093a11ac4e47ed8bfac43e6e2b9
Reed Anderson <reed.forestmonster@gmail.com>
2025-01-28 08:25:52 -0800

    fixHeroSignUpLink

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ export const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="/login"
+                href="/signup"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md hover:bg-indigo-700">
                 Get Started
               </a>
