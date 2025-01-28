11767e2ceb3e0bf204d927cb3954bfea0a1ad7a9
Reed Anderson <reed.forestmonster@gmail.com>
2025-01-28 07:56:55 -0800

    Update css for signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,14 +2,16 @@
 
 import { signup } from './actions'
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <main className="flex flex-col gap-8 sm:gap-16">
-      <section className="flex flex-col gap-4">
-        <h1>Sign Up</h1>
-        <form className="flex flex-col gap-4 max-w-md">
+      <section className="flex flex-col p-4 sm:p-6 lg:p-8">
+        <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+          Sign Up
+        </h1>
+        <form className="flex flex-col gap-4 max-w-2xl py-5">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="text-sm">
+            <label htmlFor="email" className="text-sm text-gray-500 dark:text-gray-300">
               Email:
             </label>
             <input 
@@ -17,12 +19,12 @@ export default function LoginPage() {
               name="email" 
               type="email" 
               required
-              className="input w-full bg-white/10 text-white" 
+              className="px-4 py-2 bg-white/10 dark:bg-white/5 rounded-md border border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-indigo-500" 
             />
           </div>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="text-sm">
+            <label htmlFor="password" className="text-sm text-gray-500 dark:text-gray-300">
               Password:
             </label>
             <input 
@@ -30,20 +32,20 @@ export default function LoginPage() {
               name="password" 
               type="password" 
               required
-              className="input w-full bg-white/10 text-white"
+              className="px-4 py-2 bg-white/10 dark:bg-white/5 rounded-md border border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-indigo-500"
             />
           </div>
 
           <div className="flex gap-4 mt-4">
             <button
-                formAction={signup}
-                className="btn-primary flex-1"
+              formAction={signup}
+              className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md hover:bg-indigo-700 transition-colors"
             >
-                Sign up
+              Sign Up
             </button>
           </div>
         </form>
       </section>
     </main>
-  )
+  );
 }
