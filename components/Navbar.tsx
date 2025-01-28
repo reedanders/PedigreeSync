44141aba95f691fd7f491841cff37c69cbae9bff
Reed Anderson <reed.forestmonster@gmail.com>
2025-01-28 08:32:13 -0800

    Update login styles and navbar link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
         <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
             <ThemeChanger />
             <div className="hidden mr-3 lg:flex nav__item">
-              <Link href="/signup" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
+              <Link href="/login" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
                 Get Started
               </Link>
             </div>
