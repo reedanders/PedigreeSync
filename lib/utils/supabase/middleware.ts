3460183d8066a80211287d96d4a0b00752ba8f12
Reed Anderson <reed.forestmonster@gmail.com>
2025-03-08 09:17:48 -0800

    Add test for marketing pages

--- lib/utils/supabase/middleware.ts
+++ lib/utils/supabase/middleware.ts
@@ -41,7 +41,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/signup') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/about-ebvs') &&
+    !request.nextUrl.pathname.startsWith('/roadmap') &&
     // Do not redirect if on the homepage
     request.nextUrl.pathname !== '/'
   ) {
